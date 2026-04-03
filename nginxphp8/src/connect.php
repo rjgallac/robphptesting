271f9cedfe2613d0714b803d102a0c577c765e78
rjgallac <rjgallac@hotmail.com>
2026-04-03 10:14:59 +0100

    lint

--- nginxphp8/src/connect.php
+++ nginxphp8/src/connect.php
@@ -1,3 +1,4 @@
+
 <?php
 // Connect using IP and specific port for MySQL 8
 $conn = mysqli_connect('database_robtesting', 'user', 'password', 'deniseforum', '3306');
@@ -6,4 +7,3 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-?>
