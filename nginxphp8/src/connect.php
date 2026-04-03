dfe9edb147191f8c744e199da3cab7db90cc7903
rjgallac <rjgallac@hotmail.com>
2026-04-03 10:27:39 +0100

    env vars

--- nginxphp8/src/connect.php
+++ nginxphp8/src/connect.php
@@ -1,7 +1,13 @@
 
 <?php
+// Pull variables from environment variables
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
 // Connect using IP and specific port for MySQL 8
-$conn = mysqli_connect('database_robtesting', 'user', 'password', 'deniseforum', '3306');
+$conn = mysqli_connect('database_robtesting', $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
